9fc35a2850d052b5549c9c0472156e7c0fc1fd01
abraham-laha <haverkamp-lars@hotmail.com>
2022-12-31 17:00:21 +0100

    Mache Aufgabe 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    args = Array.from(arguments);
+    let j = 0;
+    for (let arg of args) {
+        if (!j) {
+            j++;
+            continue;
+        }
+        let i = 0;
+        for (let a of arr) {
+            
+            if (a === arg) {
+                arr.splice(i, 1);
 
+                continue;
+            }
+            i++;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
