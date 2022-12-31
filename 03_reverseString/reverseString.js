0dafd2b06aa6ddcf16be085437caf7bb038735c4
abraham-laha <haverkamp-lars@hotmail.com>
2022-12-31 16:38:38 +0100

    Mache Aufgabe 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arr = word.split("");
+    let reverse = "";
+    for(let i = arr.length-1; i >= 0; i--){
+        reverse += arr[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
