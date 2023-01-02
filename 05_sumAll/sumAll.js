b34902eb98b987349539eae647d761c58ac4d9c9
abraham-laha <haverkamp-lars@hotmail.com>
2023-01-02 10:33:49 +0100

    MAche Aufgabe 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    console.log(typeof b);
+    if(a < 0 || b < 0 || typeof a != typeof 1 || typeof b != typeof 2){
+        return "ERROR";
+    }
+    else if(a > b){
+        for(let i = b; i <= a; i++){
+            sum += i;
+        }
+    }else{
+        for(let i = a; i <= b; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
