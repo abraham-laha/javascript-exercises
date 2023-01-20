2180435efdcb0a7e712f8ed0079bae7e463754e4
abraham-laha <haverkamp-lars@hotmail.com>
2023-01-20 13:52:07 +0100

    Finish ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    pal = pal.replaceAll(".","");
+    pal = pal.replaceAll(" ","");
+    pal = pal.replaceAll(",","");
+    pal = pal.replaceAll("!","");
+    pal = pal.toLowerCase();
+    for(i=0; i <= pal.length/2 - 1; i++){
+        if(pal.charAt(i)!= pal.charAt(pal.length -1 -i)) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
