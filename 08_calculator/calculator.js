c235c5f8664c579e07084a903ba3451dd62da7a7
abraham-laha <haverkamp-lars@hotmail.com>
2023-01-20 13:13:01 +0100

    Finish Ex. 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((s, item) => s += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mult, item)=> mult*=item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let fac = 1;
+  for(i=1; i <= a; i++){
+    fac *= i;
+  }
+  return fac;
 	
 };
 
