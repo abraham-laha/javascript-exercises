5f45a076a9e94f24d14452b487201983e2f8df43
abraham-laha <haverkamp-lars@hotmail.com>
2023-01-20 13:33:35 +0100

    Finish ex 9

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    let sum = [1,1];
+    for(let i = 3; i <= num; i++){
+        sum.push(sum[i-2]+sum[i-3]);
+    }
+    return sum[num];
 };
 
 // Do not edit below this line
